7d0c00b8fc7eda8e34f4aaf279431556783bcae0
phher802 <pher024@gmail.com>
2021-04-13 22:52:39 -0500

    Add port to server.js file

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', function(req, res){res.send('hello world')});
+
+app.listen(PORT, function() {console.log('Server is listening on port ${PORT}')});
+
