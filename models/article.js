296b90aa51391d19406200d712945571c245f2c2
phher802 <pher024@gmail.com>
2021-04-14 23:31:39 -0500

    Add schema to article.js file in models folder

---
+++ models/article.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const articleSchema = new mongoose.Schema({
+    category: {
+        type: String,
+        enum: ['Python', 'JavaScript', 'C#', 'Ruby', 'Java'],
+        require: [true, 'category is required']
+    },
+    title: {
+        type: String,
+        require: [true, 'Title is required']
+    }, 
+    content: {
+        type: string,
+        required: [true, "content can't be blank"]
+    }
+});
+
+module.exports = mongoose.model('Article', articleSchema);
